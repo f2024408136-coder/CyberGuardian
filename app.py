6be6cfeba2d19956a9e49b9e3e97d5a9dcd9ed5a
Mehak Javed <f2024408136@umt.edu.pk>
2026-05-25 18:41:26 +0500

    Implemented signup system with SQLite database

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -6,6 +7,45 @@ app = Flask(__name__)
 def home():
     return "CyberGuardian Running Successfully"
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+
+    if request.method == 'POST':
+
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        conn = sqlite3.connect('database/users.db')
+
+        cur = conn.cursor()
+
+        cur.execute('''
+        CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT,
+            password TEXT
+        )
+        ''')
+
+        cur.execute(
+            "INSERT INTO users(name,email,password) VALUES(?,?,?)",
+            (name, email, password)
+        )
+
+        conn.commit()
+        conn.close()
+
+        return "Signup Successful"
+
+    return render_template('signup.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
